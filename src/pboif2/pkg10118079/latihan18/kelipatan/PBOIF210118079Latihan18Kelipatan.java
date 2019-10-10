fc7e5dea521d5dd305d1da4ee764b6705ce623e3
wiranto01 <55836685+wiranto01@users.noreply.github.com>
2019-10-10 11:18:47 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118079/latihan18/kelipatan/PBOIF210118079Latihan18Kelipatan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118079.latihan18.kelipatan;
+
+/**
+ *
+ * @author user
+ * Nama : Wiranto
+ * Nim  : 10118079
+ * Kelas : IF-2
+ */
+public class PBOIF210118079Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Double i = 3.5;
+        
+        do{
+            System.out.println(i);
+            
+            i = i + 3.5;
+        } while(i<=35);
+        System.out.println();
+    }
+    
+}
